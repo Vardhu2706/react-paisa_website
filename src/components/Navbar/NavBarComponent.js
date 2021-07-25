0c05cfab41215ae2fbaef1f5e0275a307589ff4b
Vardhu2706 <ayyagari.vardhu@gmail.com>
2021-07-25 14:50:15 +0530

    Worked on Navbar.

---
+++ src/components/Navbar/NavBarComponent.js
@@ -0,0 +1,171 @@
+// NavBar Component
+// Importing Helpers
+import React from "react";
+import styled from "styled-components";
+import { Link as LinkS } from "react-scroll";
+import { Link as LinkR } from "react-router-dom";
+
+// Importing Components
+import { FaBars } from "react-icons/fa";
+
+// Styled Components
+// Nav Styles Definitions
+const Nav = styled.nav`
+  background: #000;
+  height: 80px;
+  /* margin-top: -80px; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1rem;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+
+  @media screen and (max-width: 960px) {
+    transition: 0.8s all ease;
+  }
+`;
+
+// NavBarContainer Styles Definitions
+const NavBarContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 80px;
+  z-index: 1;
+  width: 100%;
+  padding: 0 24px;
+  max-width: 1100px;
+`;
+
+// NavLogo Styles Definitions
+const NavLogo = styled(LinkS)`
+  color: #fff;
+  justify-self: flex-start;
+  cursor: pointer;
+  font-size: 1.5rem;
+  display: flex;
+  align-items: center;
+  margin-left: 24px;
+  font-weight: bold;
+  text-decoration: none;
+`;
+
+// MobileIcon Style Definitions
+const MobileIcon = styled.div`
+  display: none;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 60%);
+    font-size: 1.8rem;
+    cursor: pointer;
+    color: #fff;
+  }
+`;
+
+// NavMenu Style Definitions
+const NavMenu = styled.ul`
+  display: flex;
+  align-items: center;
+  list-style: none;
+  text-align: center;
+  margin-right: -22px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+// NavItem Style Definitions
+const NavItem = styled.li`
+  height: 80px;
+`;
+
+// NavLink Style Definitions
+const NavLink = styled(LinkS)`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+
+  &.active {
+    border-bottom: 3px solid #01bf71;
+  }
+`;
+
+// NavBtn Style Definitions
+const NavBtn = styled.nav`
+  display: flex;
+  align-items: center;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+// NavBtnLink Style Definitions
+const NavBtnLink = styled(LinkR)`
+  border-radius: 50px;
+  background: #01bf71;
+  white-space: nowrap;
+  padding: 10px 22px;
+  color: #010606;
+  font-size: 16px;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: #fff;
+    color: #010606;
+  }
+`;
+
+// Functional Component
+const NavBarComponent = () => {
+  return (
+    <>
+      <Nav>
+        <NavBarContainer>
+          <NavLogo to="/">PAISA</NavLogo>
+          {/* Menu Icon */}
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+
+          {/* Menu Items */}
+          <NavMenu>
+            <NavItem>
+              <NavLink to="about">About</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="discover">Discover</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="services">Services</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="signup">Sign Up</NavLink>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to="/signin">Sign In</NavBtnLink>
+          </NavBtn>
+        </NavBarContainer>
+      </Nav>
+    </>
+  );
+};
+
+// Default Export
+export default NavBarComponent;
