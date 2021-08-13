0ba63d086e8e9589880ac286e948083c644e1a9a
Vardhu2706 <ayyagari.vardhu@gmail.com>
2021-08-13 21:44:10 +0530

    Created Footer Component.

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -5,16 +5,23 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-// Importing Components
+// Importing Components/Media
+import {
+  FaFacebook,
+  FaTwitter,
+  FaYoutube,
+  FaLinkedin,
+  FaInstagram,
+} from "react-icons/fa";
 
 // Style Definitions
-// FooterContainer Style Definitions
-const FooterContainer = styled.div`
+// Footer Container Style Definitions
+const FooterContainer = styled.footer`
   background-color: #101522;
 `;
 
-// Footer Wrapper Style Definitions
-const FooterWrapper = styled.div`
+// Footer Wrap Style Definitions
+const FooterWrap = styled.div`
   padding: 48px 24px;
   display: flex;
   flex-direction: column;
@@ -34,7 +41,7 @@ const FooterLinksContainer = styled.div`
   }
 `;
 
-// Footer Links Wrapper  Style Definitions
+//  Footer Links Wrapper Style Definitions
 const FooterLinksWrapper = styled.div`
   display: flex;
 
@@ -50,7 +57,7 @@ const FooterLinkItems = styled.div`
   align-items: flex-start;
   margin: 16px;
   text-align: left;
-  width: 16px;
+  width: 160px;
   box-sizing: border-box;
   color: #fff;
 
@@ -76,59 +83,151 @@ const FooterLink = styled(Link)`
 
   &:hover {
     color: #01bf71;
-    transition: 0.3s ease-out;
+    transition: 0.3 ease-out;
   }
 `;
 
+// Social Media Style Definitions
+const SocialMedia = styled.section`
+  max-width: 1000px;
+  width: 100%;
+`;
+
+// Social Media Wrap Style Definitions
+const SocialMediaWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1100px;
+  margin: 40px auto 0 auto;
+
+  @media screen and (max-width: 820px) {
+    flex-direction: column;
+  }
+`;
+
+// Social Logo Style Definitions
+const SocialLogo = styled(Link)`
+  color: #fff;
+  justify-self: start;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 1.5rem;
+  display: flex;
+  align-items: center;
+  margin-bottom: 16px;
+  font-weight: bold;
+`;
+
+// Website Rights Style Definitions
+const WebsiteRights = styled.small`
+  color: #fff;
+  margin-bottom: 16px;
+`;
+
+// Social Icons Style Definitions
+const SocialIcons = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 240px;
+`;
+
+// Social Icon Link Style Definitions
+const SocialIconLink = styled.a`
+  color: #fff;
+  font-size: 24px;
+`;
+
 // Functional Component
-const FooterComponent = () => {
+function FooterComponent() {
   return (
-    <>
-      <FooterContainer>
-        <FooterWrapper>
-          <FooterLinksContainer>
-            <FooterLinksWrapper>
-              <FooterLinkItems>
-                <FooterLinkTitle>About Us</FooterLinkTitle>
-                <FooterLink to="/signin">How it works</FooterLink>
-                <FooterLink to="/signin">Testimonials</FooterLink>
-                <FooterLink to="/signin">Careers</FooterLink>
-                <FooterLink to="/signin">Investors</FooterLink>
-                <FooterLink to="/signin">Terms Of Service</FooterLink>
-              </FooterLinkItems>
-              <FooterLinkItems>
-                <FooterLinkTitle>About Us</FooterLinkTitle>
-                <FooterLink to="/signin">How it works</FooterLink>
-                <FooterLink to="/signin">Testimonials</FooterLink>
-                <FooterLink to="/signin">Careers</FooterLink>
-                <FooterLink to="/signin">Investors</FooterLink>
-                <FooterLink to="/signin">Terms Of Service</FooterLink>
-              </FooterLinkItems>
-            </FooterLinksWrapper>
-            <FooterLinksWrapper>
-              <FooterLinkItems>
-                <FooterLinkTitle>About Us</FooterLinkTitle>
-                <FooterLink to="/signin">How it works</FooterLink>
-                <FooterLink to="/signin">Testimonials</FooterLink>
-                <FooterLink to="/signin">Careers</FooterLink>
-                <FooterLink to="/signin">Investors</FooterLink>
-                <FooterLink to="/signin">Terms Of Service</FooterLink>
-              </FooterLinkItems>
-              <FooterLinkItems>
-                <FooterLinkTitle>About Us</FooterLinkTitle>
-                <FooterLink to="/signin">How it works</FooterLink>
-                <FooterLink to="/signin">Testimonials</FooterLink>
-                <FooterLink to="/signin">Careers</FooterLink>
-                <FooterLink to="/signin">Investors</FooterLink>
-                <FooterLink to="/signin">Terms Of Service</FooterLink>
-              </FooterLinkItems>
-            </FooterLinksWrapper>
-          </FooterLinksContainer>
-        </FooterWrapper>
-      </FooterContainer>
-    </>
+    <FooterContainer>
+      <FooterWrap>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+              <FooterLink to="/">How it works</FooterLink>
+              <FooterLink to="/">Testimonials</FooterLink>
+              <FooterLink to="/">Careers</FooterLink>
+              <FooterLink to="/">Investors</FooterLink>
+              <FooterLink to="/">Terms of Service</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Contact Us</FooterLinkTitle>
+              <FooterLink to="/">Contact</FooterLink>
+              <FooterLink to="/">Support</FooterLink>
+              <FooterLink to="/">Destinations</FooterLink>
+              <FooterLink to="/">Sponsorship</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>Videos</FooterLinkTitle>
+              <FooterLink to="/">Submit Video</FooterLink>
+              <FooterLink to="/">Ambassadors</FooterLink>
+              <FooterLink to="/">Agency</FooterLink>
+              <FooterLink to="/">Influencer</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Social Media</FooterLinkTitle>
+              <FooterLink to="/">Instagram</FooterLink>
+              <FooterLink to="/">Facebook</FooterLink>
+              <FooterLink to="/">YouTube</FooterLink>
+              <FooterLink to="/">Twitter</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to="/">Paisa</SocialLogo>
+            <WebsiteRights>
+              Paisa © {new Date().getFullYear()} All rights reserved.
+            </WebsiteRights>
+            <SocialIcons>
+              <SocialIconLink
+                href="//www.fb.com"
+                target="_blank"
+                aria-label="Facebook"
+              >
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink
+                href="//www.instagram.com"
+                target="_blank"
+                aria-label="Instagram"
+              >
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink
+                href="//www.twitter.com"
+                target="_blank"
+                aria-label="Twitter"
+              >
+                <FaTwitter />
+              </SocialIconLink>
+              <SocialIconLink
+                href="//www.youtube.com"
+                target="_blank"
+                aria-label="YouTube"
+              >
+                <FaYoutube />
+              </SocialIconLink>
+              <SocialIconLink
+                href="//www.linkedin.com"
+                target="_blank"
+                aria-label="LinkedIn"
+              >
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
+      </FooterWrap>
+    </FooterContainer>
   );
-};
+}
 
 // Default Export
 export default FooterComponent;
